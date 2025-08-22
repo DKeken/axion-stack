aaa76b9153f78645bb088f463c2d9c5dee8c93c6
Daniil Schovkunov <thedon2200@gmail.com>
2025-08-22 09:52:17 +0500

    chore: add "verbatimModuleSyntax" option to tsconfig files across multiple services and packages

--- apps/gateway/src/modules/gateway/gateway.service.ts
+++ apps/gateway/src/modules/gateway/gateway.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, Logger, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
+import { Injectable, Logger, type OnModuleDestroy, type OnModuleInit } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { ClientProxy, ClientProxyFactory, Transport } from '@nestjs/microservices';
 import { AuthResponseUtils, JwtValidationService } from '@repo/common';
